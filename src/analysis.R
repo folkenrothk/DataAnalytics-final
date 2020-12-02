4bcf3b54f3f72e57c283aae8c700aff8eb59c71d
Kate <>
2020-12-02 11:35:42 -0500

    added a md of potential sources

--- src/analysis.R
+++ src/analysis.R
@@ -1,8 +1,11 @@
 ## Final Project- Folkenroth/Stape: Pokemon Data
 ## Semester: Fall 2020
 
-## Read File 
+### Read File 
 
 pokemonFile <- file.choose()
 
 pokemonData <- read.csv(pokemonFile)
+
+
+#
